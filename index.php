148841da9381a8f449579b7f055652b803078002
Daltonshults <dshults@cocc.edu>
2022-05-30 08:39:52 -0700

    Fixed connection error in index.php.

--- index.php
+++ index.php
@@ -9,10 +9,10 @@ include './config.php';
 $db =  new mysqli($database_hostname, $database_username, $database_password, $database_db_name, $database_port);
 
 # If there is a connection error die
-if(mysqli_connect_errno()) {
-  echo "Failed to connect";
-  $db.die();
+if(!$db) {
+  die("Failed: ".mysqli_connection_error());
 }
+
 # Else create query
 $query = "SELECT Pid, Fname, Lname, Bday, Phone FROM PERSON;";
 
