96a0c0df742844400cf6782f6fb5c41cab7cc9f7
Daltonshults <dshults@cocc.edu>
2022-05-29 17:35:53 -0700

    Added required to input tags in join.php.

--- join.php
+++ join.php
@@ -6,37 +6,38 @@
 
     <!--First Name -->    
     <label for="Fname">First Name:</label><br>
-    <input type="text" name="Fname"><br>
+    <input type="text" name="Fname" required><br>
     
     <!--Last Name --> 
     <label for="Lname">Last Name:</label><br>
-    <input type="text" name="Lname"><br>
+    <input type="text" name="Lname" required><br>
 
     <!--Date --> 
     <label for="Date">Birthdate:</label><br>
-    <input type="text" name="Bday" placeholder="DD/MM/YYYY"><br>
+    <input type="text" name="Bday" placeholder="DD/MM/YYYY" required><br>
+    
      <!-- Phone Number -->
     <label for="Phone_number">Phone Number:</label><br>
-    <input type="text" name="Phone" placeholder="1231231234" maxlength="10" minlength="10"><br>
+    <input type="text" name="Phone" placeholder="1231231234" maxlength="10" minlength="10" required><br>
 
     <!-- Address -->
     <p class="pt-3 font-weight-bold" >Address:</p>
 
     <!-- Street -->
     <label for="Street">Street: </label><br>
-    <input type="text" name="Street" placeholder="1234 Your Address Wy."><br>
+    <input type="text" name="Street" placeholder="1234 Your Address Wy." required><br>
     
     <!-- City -->
     <label for="City">City: </label><br>
-    <input type="text" name="City"><br>
+    <input type="text" name="City" require><br>
 
     <!-- State -->
     <label for="Street">State: </label><br>
-    <input type="text" name="State" placeholder="Use two letter abbreviation" maxlength="2" minlength="2" ><br>
+    <input type="text" name="State" placeholder="Use two letter abbreviation" maxlength="2" minlength="2" required><br>
 
     <!-- Email -->
     <label for="Email">Email: </label><br>
-    <input type="text" name="Email" placeholder="JohnDoe@Kamflightschool.com" class="mb-3"><br>
+    <input type="text" name="Email" placeholder="JohnDoe@Kamflightschool.com" class="mb-3" required><br>
 
     <input type="submit">
 
