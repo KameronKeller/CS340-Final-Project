5c411693d22184d935963190f1ca4a90b445603a
Daltonshults <dshults@cocc.edu>
2022-06-01 17:53:58 -0700

    Created courses page.

--- courses.php
+++ courses.php
@@ -1,3 +1,61 @@
-<?php include "./header.php" ?>
+<!-- Include header -->
+<?php include 'header.php';
 
-<?php include "./footer.php" ?>
+#Connecting to the database 
+include './connection.php';
+
+# Else create query
+$query = "SELECT * FROM COURSES;";
+
+# Query the database
+$q = $db->query($query);
+?>
+
+<!-- Creating table and header -->
+<div class="container">
+  <table class="table mt-3">
+    <thead>
+<?php 
+#Grabbing table col names, and appending them to 
+while ($col = $q->fetch_field()) {
+  $name = $col->name;
+?>
+<th scope="col"><?=$name?></th>
+<?php
+}
+?>
+  </thead>
+<?php
+
+# While loop iterating over PERSONS returned from $query
+# Creating table
+while ($t = $q->fetch_assoc()) {
+?>
+<tr>    
+  <?php
+  # For each item in the array returned append it to the table.
+  foreach ($t as $value) {
+  ?>
+    <td><?=$value?></td>
+  <?php
+  # Closing for each bracket
+  }
+?>
+</tr>
+
+
+<?php
+# Closing while loop bracket
+}
+?>
+  </table>
+</div>
+
+<!-- Footer -->
+<?php include 'footer.php'?>
+
+
+<?php
+#close database connection
+mysqli_close($db)
+?>
