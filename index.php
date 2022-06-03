6bed45ad0de76a76b90183e96313bc6e546e1969
Kameron Keller <mrkameronkeller@gmail.com>
2022-06-02 21:22:16 -0700

    Reduce text box size

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include './header.php';
 <div class="container">
   <form action="./query_generator.php" method="POST" id="form">
     <h1><label for="Query">Query: </label><br></h1>
-    <textarea type="text-area" name="Query" cols=100 rows=20></textarea><br>
+    <textarea type="text-area" name="Query" cols=100 rows=10></textarea><br>
     <button class="btn-lg btn-dark mt-5" type="submit" form="form" value="Submit">Submit</button>    
   </form>
 </div>
