59ee0faab1529fd0bd9f4ad4402f8b8c1e6137da
Kameron Keller <mrkameronkeller@gmail.com>
2022-06-04 11:15:47 -0700

    Comment out query generator

--- query_generator.php
+++ query_generator.php
@@ -1,11 +1,16 @@
 <?php
+// Include header material
 include './header.php';
 
+// Establish connection to database
 include './connection.php';
 
+// Grab the query
 $query = $_POST['Query'];
 
+// Query the database and generate a table with result
 include './table_generation.php';
 
+// Include footer material
 include './footer.php';
 
